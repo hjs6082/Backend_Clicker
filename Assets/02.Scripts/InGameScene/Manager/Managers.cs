047199e136b6d9b5a9b5204e97802aae34b847d6
hjs6082 <70927848+hjs6082@users.noreply.github.com>
2024-08-02 04:03:18 +0900

    feat: 인게임 UI 완성. TODO: 스크립트 연결 및 스테이지 저장기능 제작

--- Assets/02.Scripts/InGameScene/Manager/Managers.cs
+++ Assets/02.Scripts/InGameScene/Manager/Managers.cs
@@ -21,7 +21,7 @@ namespace InGameScene
         [SerializeField] private InGameUI_User _userUI;
         [SerializeField] private InGameUI_Enemy _enemyUI;
         [SerializeField] private InGameUI_Stage _stageUI;
-        [SerializeField] private InGameUI_BottomUI _bottomUI;
+        [SerializeField] private InGameUI_LeftUI _leftUI;
 
         public static readonly ItemManager Item = new();
         public static readonly ProcessManager Process = new();
@@ -58,7 +58,7 @@ namespace InGameScene
 
                 Item.Init(_bulletPrefab);
 
-                _uiManager.Init(_userUI, _bottomUI, _enemyUI, _stageUI);
+                _uiManager.Init(_userUI, _leftUI, _enemyUI, _stageUI);
                 Game.Init(_player, _uiManager);
 
                 Process.Init(_player, _uiManager);
